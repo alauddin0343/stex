3e300a8102be0044e82c3ce5a33316ab0ee19d82
Petr Weissar <weissar.petr@gmail.com>
2016-07-14 09:49:30 +0800

    Functional MapView

--- app/src/main/java/cz/uhk/cityunavigate/MapActivity.java
+++ app/src/main/java/cz/uhk/cityunavigate/MapActivity.java
@@ -1,9 +1,31 @@
 package cz.uhk.cityunavigate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MapActivity extends AppCompatActivity {
+
+    MapView mapView;
+    GoogleMap map;
+
+    private void centerMapToLatLng(LatLng latLng){
+        if(map != null){
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(22.336292, 114.173910), 10);
+            map.animateCamera(cameraUpdate);
+        }
+    }
+    private void putMarkerOnMap(){
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +33,50 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
 
         setTitle("Map");
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                //map.setMyLocationEnabled(true);
+                MapsInitializer.initialize(getApplicationContext());
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(22.336292, 114.173910), 10);
+
+                map.animateCamera(cameraUpdate);
+            }
+        });
+
     }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
 }
