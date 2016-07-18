a80501d16bb2d636ff1f46d4bb9fd4fbb9a2f9f5
Karel Petranek <karel.petranek@gmail.com>
2016-07-18 11:01:30 +0800

    Sign up to invited groups on login

--- app/src/main/java/cz/uhk/cityunavigate/StartupActivity.java
+++ app/src/main/java/cz/uhk/cityunavigate/StartupActivity.java
@@ -2,16 +2,18 @@ package cz.uhk.cityunavigate;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Decides whether to switch to login activity or directly to main activity
@@ -23,7 +25,70 @@ public class StartupActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser loggedInUser = firebaseAuth.getCurrentUser();
+                if (loggedInUser == null)
+                    return;
+
+                DatabaseReference invitations = FirebaseDatabase.getInstance()
+                        .getReference("invitations")
+                        .child(Util.MD5(loggedInUser.getEmail()))
+                        .child("groups");
+                invitations.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            String groupId = snapshot.getKey();
+                            DatabaseReference groupUsers = FirebaseDatabase.getInstance()
+                                    .getReference("groups")
+                                    .child(groupId)
+                                    .child("users")
+                                    .child(loggedInUser.getUid());
+                            groupUsers.setValue(System.currentTimeMillis());
+
+                            DatabaseReference userGroups = FirebaseDatabase.getInstance()
+                                    .getReference("users")
+                                    .child(loggedInUser.getUid())
+                                    .child("groups")
+                                    .child(groupId);
+                            userGroups.setValue(System.currentTimeMillis());
+
+                            snapshot.getRef().removeValue();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                final DatabaseReference userRef = FirebaseDatabase.getInstance()
+                        .getReference("users")
+                        .child(loggedInUser.getUid());
+
+                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (!dataSnapshot.exists()) {
+                            // Add the user if not present
+                            userRef.child("created").setValue(System.currentTimeMillis());
+                            userRef.child("email").setValue(loggedInUser.getEmail());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
             startActivityForResult(
                     AuthUI.getInstance().createSignInIntentBuilder()
